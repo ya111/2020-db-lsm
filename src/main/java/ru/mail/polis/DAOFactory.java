986dd09e8938dad176eaa3475eea44a60b01b12b
ya111 <ppanichkin@ya.ru>
2020-04-21 20:18:56 +0300

    codeclimate fix

--- src/main/java/ru/mail/polis/DAOFactory.java
+++ src/main/java/ru/mail/polis/DAOFactory.java
@@ -17,7 +17,7 @@
 package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
-import ru.mail.polis.ya111.myDAO;
+import ru.mail.polis.ya111.DAOimpl;
 
 import java.io.File;
 import java.io.IOException;
@@ -54,6 +54,6 @@ public final class DAOFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        return new myDAO();
+        return new DAOimpl();
     }
 }
---
+++ src/main/java/ru/mail/polis/ya111/DAOimpl.java
@@ -0,0 +1,37 @@
+package ru.mail.polis.ya111;
+
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.DAO;
+import ru.mail.polis.Record;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class DAOimpl implements DAO {
+
+    private final SortedMap<ByteBuffer, Record> data = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
+        return data.tailMap(from).values().iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
+        data.put(key, Record.of(key, value));
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
+        data.remove(key);
+    }
+
+    @Override
+    public void close() throws IOException {
+        //TODO
+    }
+}
