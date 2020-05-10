05b9c71a37ef0908c19258c1844aad9855e4ee59
ya111 <ppanichkin@ya.ru>
2020-05-10 13:38:22 +0300

    cell fix

--- src/main/java/ru/mail/polis/ya111/Cell.java
+++ src/main/java/ru/mail/polis/ya111/Cell.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.ByteBuffer;
 import java.util.Comparator;
 
-public final class Cell {
+final class Cell {
     public static final Comparator<Cell> COMPARATOR
             = Comparator.comparing(Cell::getKey).thenComparing(Cell::getValue);
 
